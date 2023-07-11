12234516b8089899644b087ad365882f1b61c5da
Constantin-Marius Costescu <101038739+CostescuMarius@users.noreply.github.com>
2023-07-11 14:07:12 +0300

    C3 - Project Setup Update

--- oxygen-account/src/main/java/com/oxygenxml/account/OxygenAccountApplication.java
+++ oxygen-account/src/main/java/com/oxygenxml/account/OxygenAccountApplication.java
@@ -1,4 +1,4 @@
-package com.oxygenxml.demo;
+package com.oxygenxml.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
