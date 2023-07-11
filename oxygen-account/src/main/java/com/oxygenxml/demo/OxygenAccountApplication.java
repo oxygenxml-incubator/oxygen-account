7e5e23bc0692bfc8502d7b88e6d6c4b02fb97a50
Constantin-Marius Costescu <101038739+CostescuMarius@users.noreply.github.com>
2023-07-11 09:23:15 +0300

    Commit - Project Setup

---
+++ oxygen-account/src/main/java/com/oxygenxml/demo/OxygenAccountApplication.java
@@ -0,0 +1,13 @@
+package com.oxygenxml.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxygenAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxygenAccountApplication.class, args);
+	}
+
+}
---
+++ oxygen-account/src/test/java/com/oxygenxml/demo/OxygenAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxygenxml.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxygenAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
