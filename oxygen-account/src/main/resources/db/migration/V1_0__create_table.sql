93cdd6b25e1b917f0e2834cbf3471d61941ec03e
Constantin-Marius Costescu <101038739+CostescuMarius@users.noreply.github.com>
2023-07-13 14:53:46 +0300

    C9 - Update DB structure to register users

--- oxygen-account/src/main/resources/db/migration/V1_0__create_table.sql
+++ oxygen-account/src/main/resources/db/migration/V1_0__create_table.sql
@@ -1,7 +1,7 @@
-CREATE TABLE USER (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    registration_date DATE NOT NULL,
-    password VARCHAR(50) NOT NULL
+    username VARCHAR(511) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    password VARCHAR(255) NOT NULL
 );
