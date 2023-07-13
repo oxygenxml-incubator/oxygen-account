ceaccd4ff7f2da0024c8b34a94731e48d9f6ae06
Mateescu Denis <118893406+matrex32@users.noreply.github.com>
2023-07-13 15:12:52 +0300

    C10 - Update Java documentation for  database entity model

--- oxygen-account/src/main/java/com/oxygenxml/model/User.java
+++ oxygen-account/src/main/java/com/oxygenxml/model/User.java
@@ -6,9 +6,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+/**
+
+The User class represents a user entity in the system.
+
+It is annotated with the necessary annotations for persistence.
+
+The class includes basic user information such as username, email, registration date, and password.
+*/
+
 @Data
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
 	
 	@Id
