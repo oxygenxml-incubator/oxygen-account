3beeceb023200de96e43a851750e9697ed99f63f
Constantin-Marius Costescu <101038739+CostescuMarius@users.noreply.github.com>
2023-07-14 16:22:53 +0300

    Update pom.xml and Test

--- oxygen-account/src/test/java/com/oxygenxml/account/OxygenAccountApplicationTests.java
+++ oxygen-account/src/test/java/com/oxygenxml/account/OxygenAccountApplicationTests.java
@@ -3,7 +3,7 @@ package com.oxygenxml.account;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = OxygenAccountApplicationTests.class)
 class OxygenAccountApplicationTests {
 
 	@Test
