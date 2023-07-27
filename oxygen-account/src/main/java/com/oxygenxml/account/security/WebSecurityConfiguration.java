03fad8a9f3aa2a6bd7a976439604cb99ad845b6e
Mateescu Denis <118893406+matrex32@users.noreply.github.com>
2023-07-27 15:03:13 +0300

    #26
    Am modificat metoda BCryptPasswordEncoder din clasa WebSecurityConfigutation

--- oxygen-account/src/main/java/com/oxygenxml/account/security/WebSecurityConfiguration.java
+++ oxygen-account/src/main/java/com/oxygenxml/account/security/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -19,7 +20,7 @@ public class WebSecurityConfiguration  {
 	 * @return a BCryptoPasswordEncoder
 	 */
 	@Bean
-	public BCryptPasswordEncoder encoder() {
+	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
