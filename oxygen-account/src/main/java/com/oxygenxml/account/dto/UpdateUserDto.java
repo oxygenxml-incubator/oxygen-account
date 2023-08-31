302a10fb6cf8df2897f31915c57866595b2191e5
Mateescu Denis <118893406+matrex32@users.noreply.github.com>
2023-08-31 09:51:42 +0300

    Added javadoc for UpdateUserDto

--- oxygen-account/src/main/java/com/oxygenxml/account/dto/UpdateUserDto.java
+++ oxygen-account/src/main/java/com/oxygenxml/account/dto/UpdateUserDto.java
@@ -5,11 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents a Data Transfer Object for updating the user's name.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateUserDto {
 	
+	/**
+	 * The new name for the user
+	 */
 	@NotBlank
 	private String name;
 }
