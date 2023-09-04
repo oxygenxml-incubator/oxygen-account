18cf125144e0221114d1c8e7feea048a9026f896
Constantin-Marius Costescu <101038739+CostescuMarius@users.noreply.github.com>
2023-09-04 10:09:55 +0300

    #45 Resolve Github Action error

--- oxygen-account/src/main/java/com/oxygenxml/account/dto/UpdateUserNameDto.java
+++ oxygen-account/src/main/java/com/oxygenxml/account/dto/UpdateUserNameDto.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UpdateUserDto {
+public class UpdateUserNameDto {
 	
 	/**
 	 * The new name for the user
