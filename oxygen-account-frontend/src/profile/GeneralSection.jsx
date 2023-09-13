b8427139f2dd0625268f4c932f61804d748fa76a
Constantin-Marius Costescu <101038739+CostescuMarius@users.noreply.github.com>
2023-09-13 17:09:12 +0300

    #51 Refactor "ProfileCard"

---
+++ oxygen-account-frontend/src/profile/GeneralSection.jsx
@@ -0,0 +1,215 @@
+import React from "react";
+
+import OxygenAvatar from "../shared/OxygenAvatar.jsx";
+
+import { Grid, TextField, Button, LinearProgress, Typography } from "@mui/material";
+
+import { useState } from "react";
+
+
+export default function GeneralSection({ currentUserData, updateCurrentUser, showMessage }) {
+    // State variable indicating whether the edit mode is active.
+    const [isEditActive, setIsEditActive] = useState(false);
+
+    // State variable for holding the edited user name.
+    const [editedUserName, setEditedUserName] = useState(currentUserData.name);
+
+    // State variable for holding the error message related to the edited name.
+    const [editedNameError, setEditedNameError] = useState('');
+
+    // State variable indicating whether the edit submission is in progress.
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    /**
+     * Handle input change event for text field.
+     */
+    const handleInputChange = (event) => {
+        const { id, value } = event.target;
+
+        if (id === "name-info") {
+            setEditedUserName(value);
+
+            if (editedNameError !== '') {
+                setEditedNameError('');
+            }
+        }
+    }
+
+    /**
+     *Activates the edit mode when the edit button is clicked.
+     */
+    const handleClickEditButton = () => {
+        setIsEditActive(true);
+    };
+
+    /**
+     * Cancels the edit mode and reverts any unsaved changes when the cancel edit button is clicked.
+     */
+    const handleClickCancelEditButton = () => {
+        // Revert name if it was changed
+        if (editedUserName !== currentUserData.name) {
+            setEditedUserName(currentUserData.name);
+            setEditedNameError('');
+        }
+
+        setIsEditActive(false);
+    };
+
+    /**
+     * Validate edited inputs.
+     *
+     * @returns {boolean} true if the edited inputs are valid.
+     */
+    const validateEditedInputs = () => {
+        let isEditedNameValid = editedUserName.trim() !== '';
+        setEditedNameError(isEditedNameValid ? '' : 'Please provide a non-empty value.');
+
+        return isEditedNameValid;
+    }
+
+    /**
+     * Send an edit name request to the server.
+     */
+    const sendEditNameRequest = () => {
+        setIsLoading(true);
+
+        return fetch('api/users/profile', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(editedUserName),
+        }).then((response) => {
+            if (response.ok) {
+                setIsLoading(false);
+                setIsEditActive(false);
+
+                showMessage(true, 'Profile has been updated!')
+            }
+            return response.json();
+        }).then((data) => {
+            if (data.errors) {
+                if (data.errors[0].fieldName === 'name') {
+                    setEditedNameError(data.errors[0].errorMessage);
+                }
+            } else if (data.errorMessage) {
+                throw new Error(data.errorMessage);
+            } else {
+                updateCurrentUser(data);
+            }
+        }).catch(error => {
+            setIsLoading(false);
+
+            showMessage(false, error.name == "TypeError" ? "The connection could not be established." : error.message);
+        });
+    };
+
+    /**
+     * Initiates the save process if changes are valid when the save button is clicked.
+     */
+    const handleClickSaveButton = () => {
+        if (editedUserName === currentUserData.name) {
+            setIsEditActive(false);
+        } else if(validateEditedInputs()) {
+            sendEditNameRequest();
+        }
+    }
+
+    return (
+        < Grid item container direction='column' gap='20px' >
+            <Grid item style={{ padding: '10px' }}>
+                <Typography variant="h6">
+                    General
+                </Typography>
+            </Grid>
+            <Grid item container gap='10px'>
+                {/* Display user avatar */}
+                <Grid item style={{ maxWidth: 'fit-content' }}>
+                    <OxygenAvatar
+                        name={currentUserData.name}
+                        size={"100px"}
+                    />
+                </Grid>
+
+                {/* Display user info */}
+                <Grid item container style={{ flexGrow: 1, flexBasis: 0 }} direction='column' gap='15px'>
+                    {/* Input field for showing and editing the name */}
+                    <Grid item>
+                        <TextField
+                            id="name-info"
+                            label="Name"
+                            variant="outlined"
+                            value={editedUserName}
+                            InputProps={{ readOnly: !isEditActive }}
+                            onChange={handleInputChange}
+                            error={Boolean(editedNameError)}
+                            helperText={editedNameError}
+                            fullWidth
+                        />
+                    </Grid>
+
+                    {/* Display user's email */}
+                    <Grid item>
+                        <TextField
+                            id="email-info"
+                            label="Email"
+                            variant="outlined"
+                            value={currentUserData.email}
+                            InputProps={{ readOnly: true }}
+                            fullWidth
+                        />
+                    </Grid>
+                </Grid>
+            </Grid>
+
+            {/* Conditionally render buttons based on Edit Mode */}
+            <Grid item container justifyContent="flex-end">
+                {isEditActive ? (
+                    <Grid item container style={{ maxWidth: 'fit-content' }} gap='10px'>
+                        {/* Cancel button */}
+                        <Grid item>
+                            <Button
+                                variant="contained"
+                                onClick={handleClickCancelEditButton}
+                                disabled={isLoading}
+                                style={{ background: 'lightgray', color: 'black' }}>
+                                Cancel
+                            </Button>
+                        </Grid>
+
+                        {/* Save button */}
+                        <Grid item>
+                            <Button id="save-button"
+                                variant="contained"
+                                disabled={isLoading}
+                                onClick={handleClickSaveButton}>
+                                Save
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                ) : (
+                    // Edit button
+                    <Grid item>
+                        <Button
+                            id="edit-button"
+                            variant="contained"
+                            onClick={handleClickEditButton}
+                            disabled={currentUserData.status === 'deleted'}>
+                            Edit
+                        </Button>
+                    </Grid>
+                )}
+            </Grid>
+
+            {/* Display a progress bar during edit submission */}
+            {
+                isLoading &&
+                <Grid item xs>
+                    <LinearProgress />
+                </Grid>
+            }
+        </Grid >
+    );
+};
---
+++ oxygen-account-frontend/src/profile/SecuritySection.jsx
@@ -0,0 +1,274 @@
+import React from "react";
+
+import { useState } from "react";
+
+import { Grid, TextField, Button, Typography, LinearProgress } from "@mui/material";
+
+export default function SecuritySection({currentUserData, showMessage}) {
+    // State variable indicating whether the change password view is active or not.
+    const [isChangePasswordViewActive, setIsChangePasswordViewActive] = useState(false);
+
+    // State variable for holding the current password.
+    const [currentPassword, setCurrentPassword] = useState('');
+
+    // State variable for holding the new password.
+    const [newPassword, setNewPassword] = useState('');
+
+    // State variable for confirming the new password.
+    const [confirmNewPassword, setConfirmNewPassword] = useState('');
+
+    // State variable for holding the error message related to the current password.
+    const [currentPasswordError, setCurrentPasswordError] = useState('');
+
+    // State variable for holding the error message related to the new password.
+    const [newPasswordError, setNewPasswordError] = useState('');
+
+    // State variable for holding the error message for confirming the new password.
+    const [confirmNewPasswordError, setConfirmNewPasswordError] = useState('');
+
+    // State variable indicating whether a change password submission is in progress.
+    const [isLoading, setIsLoading] = useState(false);
+
+    /**
+     * Handle input change event for text field.
+     */
+    const handleInputChange = (event) => {
+        const { id, value } = event.target;
+
+        if (id === "current-password") {
+            setCurrentPassword(value);
+
+            if (currentPasswordError !== '') {
+                setCurrentPasswordError('');
+            }
+        }
+
+        else if (id === "new-password") {
+            setNewPassword(value);
+
+            if (newPasswordError !== '') {
+                setNewPasswordError('');
+            }
+        }
+
+        else if (id === "confirm-new-password") {
+            setConfirmNewPassword(value);
+
+            if (confirmNewPasswordError !== '') {
+                setConfirmNewPasswordError('');
+            }
+        }
+    }
+
+    /**
+     * Activates the change password view.
+     */
+    const handleChangePasswordClick = () => {
+        setIsChangePasswordViewActive(true);
+    }
+
+    /**
+     * Clears all password-related fields and errors.
+     */
+    const clearPasswordFileds = () => {
+        setCurrentPassword('');
+        setNewPassword('');
+        setConfirmNewPassword('');
+
+        setCurrentPasswordError('');
+        setNewPasswordError('');
+        setConfirmNewPasswordError('');
+    }
+
+    /**
+     * Validates the new password.
+     * @returns {boolean} - True if the new password is valid; otherwise, false.
+     */
+    const validateNewPassword = () => {
+        let isNewPasswordValid = /^.{8,}$/.test(newPassword.trim());
+        let isConfirmNewPasswordValid = newPassword === confirmNewPassword;
+        let isNewPasswordUsed = currentPassword === newPassword;
+
+        if (!isNewPasswordValid) {
+            setNewPasswordError('Input field is too short. Please enter a longer value.');
+        } else if (isNewPasswordUsed) {
+            setNewPasswordError('The new password is the same as the old one.');
+        } else {
+            setNewPasswordError('');
+        }
+        setConfirmNewPasswordError(isConfirmNewPasswordValid ? '' : 'Passwords do not match.');
+
+        return isNewPasswordValid && isConfirmNewPasswordValid && !isNewPasswordUsed;
+    }
+
+    /**
+     * Deactivates the change password view and clears fields.
+     */
+    const handleCancelChangePasswordClick = () => {
+        setIsChangePasswordViewActive(false);
+
+        clearPasswordFileds();
+    }
+
+    /**
+     * Sends a request to change the password.
+     */
+    const sendChangePasswordRequest = () => {
+        setIsLoading(true);
+
+        const updatePasswordInfo = {
+            oldPassword: currentPassword.trim(),
+            newPassword: newPassword.trim(),
+        };
+
+        return fetch('api/users/password', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatePasswordInfo),
+        }).then((response) => {
+            if (response.ok) {
+                setIsLoading(false);
+                setIsChangePasswordViewActive(false);
+                clearPasswordFileds();
+
+                showMessage(true, 'The password has been changed successfully.');
+            }
+            return response.json();
+        }).then((data) => {
+            if (data.errors) {
+                data.errors.forEach(error => {
+                    switch (error.fieldName) {
+                        case "oldPassword":
+                            setCurrentPasswordError(error.errorMessage);
+                            break;
+                        case "newPassword":
+                            setNewPasswordError(error.errorMessage);
+                            break;
+                    }
+                });
+            } else if (data.errorMessage) {
+                throw new Error(data.errorMessage);
+            }
+        }).catch(error => {
+            setIsLoading(false);
+
+            showMessage(false, error.name == "TypeError" ? "The connection could not be established." : error.message);
+        });
+    };
+
+    /**
+     * Handles the click event for saving the new password.
+     * Validates the new password and initiates the change password request if valid.
+     */
+    const handleSavePasswordClick = () => {
+        if (validateNewPassword()) {
+            sendChangePasswordRequest();
+        }
+    }
+
+    return (
+        <Grid item container direction='column' style={{ borderTop: "1px solid #333" }} gap='15px'>
+            {/* Security Title */}
+            <Grid item style={{ padding: '10px' }}>
+                <Typography variant="h6">
+                    Security
+                </Typography>
+            </Grid>
+
+            {/* Change Password Input Fields */}
+            {isChangePasswordViewActive &&
+                <Grid item container direction="column" gap='15px'>
+                    {/* Current Password Input */}
+                    <Grid item>
+                        <TextField
+                            id="current-password"
+                            label="Current Password"
+                            type="password"
+                            value={currentPassword}
+                            onChange={handleInputChange}
+                            error={Boolean(currentPasswordError)}
+                            helperText={currentPasswordError}
+                            fullWidth
+                        />
+                    </Grid>
+
+                    {/* New Password Input */}
+                    <Grid item>
+                        <TextField
+                            id="new-password"
+                            label="New Password"
+                            type="password"
+                            value={newPassword}
+                            onChange={handleInputChange}
+                            error={Boolean(newPasswordError)}
+                            helperText={newPasswordError}
+                            fullWidth
+                        />
+                    </Grid>
+
+                    {/* Confirm New Password Input */}
+                    <Grid item>
+                        <TextField
+                            id="confirm-new-password"
+                            label="Confirm New Password"
+                            type="password"
+                            value={confirmNewPassword}
+                            onChange={handleInputChange}
+                            error={Boolean(confirmNewPasswordError)}
+                            helperText={confirmNewPasswordError}
+                            fullWidth
+                        />
+                    </Grid>
+                </Grid>}
+
+            {/* Conditionally render buttons based on Change Password Mode */}
+            <Grid item container justifyContent="flex-end">
+                <Grid item container justifyContent="flex-end">
+                    {isChangePasswordViewActive ? (
+                        <Grid item container style={{ maxWidth: 'fit-content' }} gap='10px'>
+                            { /*  Save and Cancel Buttons */}
+                            <Grid item>
+                                <Button
+                                    variant="contained"
+                                    onClick={handleCancelChangePasswordClick}
+                                    style={{ background: 'lightgray', color: 'black' }}>
+                                    Cancel
+                                </Button>
+                            </Grid>
+
+                            <Grid item>
+                                <Button
+                                    id="save-password-button"
+                                    disabled={currentPassword === '' || newPassword === '' || confirmNewPassword === ''}
+                                    variant="contained"
+                                    onClick={handleSavePasswordClick}>
+                                    Save Password
+                                </Button>
+                            </Grid>
+                        </Grid>
+
+                    ) : (
+                        // Change Password Button
+                        <Grid item>
+                            <Button
+                                id="change-password-button"
+                                variant="contained"
+                                onClick={handleChangePasswordClick}
+                                disabled={(currentUserData.status === 'deleted')}>
+                                Change Password
+                            </Button>
+                        </Grid>
+                    )}
+                </Grid>
+            </Grid>
+
+            {/* Display a Progress Bar during Change Password Submission */}
+            {isLoading &&
+                <Grid item xs>
+                    <LinearProgress />
+                </Grid>}
+        </Grid>
+    )
+};
