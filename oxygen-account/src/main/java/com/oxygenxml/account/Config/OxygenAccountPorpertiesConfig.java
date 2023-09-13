cded24ad8293c80dbffbd9f5adc3d4ad93cdb11f
Mateescu Denis <118893406+matrex32@users.noreply.github.com>
2023-09-13 11:28:15 +0300

    Adding Setter for OxygenAccountPropertiesConfig

--- oxygen-account/src/main/java/com/oxygenxml/account/Config/OxygenAccountPorpertiesConfig.java
+++ oxygen-account/src/main/java/com/oxygenxml/account/Config/OxygenAccountPorpertiesConfig.java
@@ -4,11 +4,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Configuration class that binds the properties defined in the application's properties file prefixed with "oxygen.account" to this class's fields.
  */
 @Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "oxygen.account")
 public class OxygenAccountPorpertiesConfig {
